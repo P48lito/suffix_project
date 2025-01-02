6cecc9e70caf74723459f194dd5d15c6dc80887a
_q_ryy_ <lesnowolskip@gmail.com>
2025-01-02 15:24:38 +0100

    I've made better version of deleting suffixes :)

--- main4.py
+++ main4.py
@@ -7,17 +7,32 @@ dir = 'C:/Users/lesno/Desktop/PProjects/suffix_project/music/'
 
 # Choose only one line uncommented below
 for filename in os.listdir(dir):
-    # os.rename(dir + filename, dir + filename.replace(' [ ezmp3.cc ].mp3', '.mp3'))
-    # os.rename(dir + filename, dir + filename.replace('(Lyrics)', ''))
-    # os.rename(dir + filename, dir + filename.replace('(Audio)', ''))
-    # os.rename(dir + filename, dir + filename.replace('HQ!', ''))
-    # os.rename(dir + filename, dir + filename.replace('(Remastered)', ''))
-    # os.rename(dir + filename, dir + filename.replace('(Official Audio)', ''))
-    # os.rename(dir + filename, dir + filename.replace('(Official Video)', ''))
-    # os.rename(dir + filename, dir + filename.replace('(Official Audio)', ''))
-    # os.rename(dir + filename, dir + filename.replace('(Radio Edit)', ''))
-    # os.rename(dir + filename, dir + filename.replace('(Album Version)', ''))
-    # os.rename(dir + filename, dir + filename.replace('(Official Audio)', ''))
-    # os.rename(dir + filename, dir + filename.replace(' (Official Video)', ''))
-    # os.rename(dir + filename, dir + filename.replace(' (Original Version)', ''))
-    # os.rename(dir + filename, dir + filename.replace(' (Original Version)', ''))
+    os.rename(dir + filename, dir + filename.replace(' [ ezmp3.cc ].mp3', '.mp3'))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Lyrics)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Audio)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('HQ!', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Remastered)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Official Audio)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Official Video)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Official Audio)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Radio Edit)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Album Version)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace('(Official Audio)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace(' (Official Video)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace(' (Original Version)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace(' (Original Version)', ''))
+for filename in os.listdir(dir):
+    os.rename(dir + filename, dir + filename.replace(' Cover', ''))
